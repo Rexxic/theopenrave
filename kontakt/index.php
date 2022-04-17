b52a0cbcd35d62cdbc07b18ee6757b194977bd8f
Cedric Seiz <cedricseiz@gmail.com>
2022-04-17 20:55:27 +0200

    Edited kontakt page

--- kontakt/index.php
+++ kontakt/index.php
@@ -12,7 +12,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/include/nav.php");
     <h3 class="kontaktheader">
         Herzlich Willkommen, wir sind The Open Rave.
     </h3>
-    <p class="text">
+    <p class="kontakttext">
         Wir veranstalten aus Leidenschaft Raves in Heilbronn und Umgebung. Techno ist eine
         Gemeinschaft, die wir sehr schätzen und jeder ist bei uns willkommen. Seit 2020
         veranstalten wir Outdoor Raves und gewinnen mit jedem Event an Erfahrung und
@@ -20,19 +20,19 @@ include($_SERVER['DOCUMENT_ROOT'] . "/include/nav.php");
         auch mit Veranstaltungen in Indoor Locations durchzustarten und unsere regionale Technoszene
         um unvergessliche Abende zu bereichern.
     </p>
-    <p class="text">
+    <p class="kontakttext">
         Wenn ihr immer die neuesten Infos zu unseren Events bekommen wollt,
         kommt in eine unserer Whatsapp-Gruppen.
     </p>
-    <p class="text">
+    <p class="kontakttext">
         <a href="https://chat.whatsapp.com/Dlil2R9OhjV5wLyqivORja">Gruppe 1</a>
         <a href="https://chat.whatsapp.com/D52t3DjmE2YC78WgCGLfo0">Gruppe 2</a>
     </p>
-    <p class="text">
+    <p class="kontakttext">
         Wenn ihr Fragen oder Anregungen habt könnt ihr uns gerne kontaktieren.
     </p>
-    <p class="text">
-        <a href="mailto: kontakt@theopenrave.de">kontakt@theopenrave.de</a>
+    <p class="kontakttext">
+        <a href="mailto: kontakt@theopenrave.de" class="kontaktlink">kontakt@theopenrave.de</a>
     </p>
 </main>
 <?php

30db09eba9f2a88f04c5ad6d1dee35ead93ff363
Cedric Seiz <cedricseiz@gmail.com>
2022-04-17 23:58:12 +0200

    gscheide schriftart neigmacht

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@ $today = date('d.m.Y');
 $now = date('H:i:s'); ?>
 
 <footer>
-    <span>Created by Rexxic</span>
+    <span style="text-align: right">Created by Rexxic</span>
     <a href="https://github.com/Rexxic/theopenrave"><img src="/media/logos/GitHub-Mark-Light-64px.png" alt="GitHub" width="32vw"></a>
-    <span> Seite abgerufen am <?=$today?> um <?=$now?> Uhr.</span>
+    <span style="text-align: left"> Seite abgerufen am <?=$today?> um <?=$now?> Uhr.</span>
 </footer>

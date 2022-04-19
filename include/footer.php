326cc2f43bb73aed870affe0f5d8034d5a0f9518
Cedric Seiz <cedricseiz@gmail.com>
2022-04-19 21:48:25 +0200

    gscheide schriftart gscheid neigmacht

--- include/footer.php
+++ include/footer.php
@@ -4,8 +4,7 @@ $now = date('H:i:s'); ?>
 
 <footer>
     <span style="text-align: right">Created by Rexxic</span>
-    <a class="gitlogo" href="https://github.com/Rexxic/theopenrave"><img height="100%"
-                                                                         src="/media/logos/GitHub-Mark-Light-64px.png"
-                                                                         alt="GitHub"></a>
-    <span style="text-align: left"> Seite abgerufen am <?=$today?> um <?=$now?> Uhr.</span>
+    <a class="gitlogo" href="https://github.com/Rexxic/theopenrave">
+        <img height="100%" src="/media/logos/GitHub-Mark-Light-64px.png" alt="GitHub"></a>
+    <span style="text-align: left">Seite abgerufen am <?= $today ?> um <?= $now ?> Uhr.</span>
 </footer>

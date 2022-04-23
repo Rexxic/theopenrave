9616753e2a5a1f0d5d913afb2c377718cf71c849
Cedric Seiz <cedricseiz@gmail.com>
2022-04-23 21:25:05 +0200

    Search Engine Optimisation

--- include/head.php
+++ include/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <meta property="og:image" content="<?= $_SERVER['DOCUMENT_ROOT'] ?>/media/TorLogo.png">
+    <meta property="og:image" content="/media/TorLogo.png">
     <meta property="og:description" content="Raves Heilbronn & Umgebung - Techno, HardTech, DnB">
     <meta property="og:title" content="The Open Rave">
     <meta name="robots" content="max-image-preview:large">

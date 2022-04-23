74d87f7c594168412ac828d3cddb2d17ae413ea6
Cedric Seiz <cedricseiz@gmail.com>
2022-04-23 18:25:50 +0200

    Added viewport and robots meta tag

--- include/head.php
+++ include/head.php
@@ -3,6 +3,8 @@
     <meta property="og:image" content="<?= $_SERVER['DOCUMENT_ROOT'] ?>/media/TorLogo.png">
     <meta property="og:description" content="Raves Heilbronn & Umgebung - Techno, HardTech, DnB">
     <meta property="og:title" content="The Open Rave">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="max-image-preview:large">
     <title>The Open Rave</title>
     <link rel="stylesheet" href="/fonts/fonts.php">
     <link rel="stylesheet" href="/styles.css">

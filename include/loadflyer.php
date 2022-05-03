db9d500bbbf34748eb99e22fe580f132c85ecb7a
Cedric Seiz <cedricseiz@gmail.com>
2022-05-03 14:42:06 +0200

    Hompage für Mobilat Aktualisiert

--- include/loadflyer.php
+++ include/loadflyer.php
@@ -2,6 +2,10 @@
 if (!empty($dir)) {
     $scanned_dir = array_diff(scandir($_SERVER['DOCUMENT_ROOT'] . $dir), array('..', '.'));
     foreach ($scanned_dir as $file) {
-        echo '<img class="flyer" src="' . $dir . '\\' . $file . '" alt="' . $file . '">';
+        if (str_contains($file, ".png")) {
+            echo '<img class="flyer" src="' . $dir . '\\' . $file . '" alt="' . $file . '">';
+        } elseif (str_contains($file, ".php")) {
+            include($_SERVER['DOCUMENT_ROOT'] . $dir . '\\' . $file);
+        }
     }
 }
---
+++ media/homepage/Mobilat27052020-1.php
@@ -0,0 +1,21 @@
+<?php
+$eventDate = new DateTime('2022-05-27T23:00:00');
+$now = new DateTime();
+$interval = $now->diff($eventDate, true);
+?>
+<div class="kontakt">
+    <h3 class="kontaktheader">
+        The Open Night: <a href="https://mobilat.club">Mobilat</a>
+    </h3>
+    <p class="kontakttext">
+        <?php
+        if ($eventDate >= $now) {
+            echo 'In ';
+            if ($interval->days > 0) echo $interval->days . ' Tagen und ';
+            echo $interval->h . ' Stunden ist es so weit.';
+        }
+        ?>
+        Am <?= $eventDate->format('d.m.Y') ?> um <?= $eventDate->format('H') ?>
+        Uhr dürfen wir euch zu unserem ersten eigenen Event im Mobilat Heilbronn begrüßen.
+    </p>
+</div>

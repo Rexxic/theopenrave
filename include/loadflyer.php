f5e94cb20ca4b22a1c2a9489f80a96c85cb0dace
Rxc <cedricseiz@gmail.com>
2022-05-04 03:56:41 +0200

    Added indicator for current page

--- include/loadflyer.php
+++ include/loadflyer.php
@@ -2,9 +2,9 @@
 if (!empty($dir)) {
     $scanned_dir = array_diff(scandir($_SERVER['DOCUMENT_ROOT'] . $dir), array('..', '.'));
     foreach ($scanned_dir as $file) {
-        if (str_contains($file, ".png")) {
+        if (strpos($file, ".png") !== false) {
             echo '<img class="flyer" src="' . $dir . '\\' . $file . '" alt="' . $file . '">';
-        } elseif (str_contains($file, ".php")) {
+        } elseif (strpos($file, ".php") !== false) {
             include($_SERVER['DOCUMENT_ROOT'] . $dir . '\\' . $file);
         }
     }

4a4fa9aa27955613f6d670c3e43378b42a4a3404
Cedric Seiz <cedricseiz@gmail.com>
2022-05-09 14:48:44 +0200

    Fix of logic in counter.php

--- include/counter.php
+++ include/counter.php
@@ -9,7 +9,7 @@ if(file_exists($filePath)) {
     $counter = file_get_contents($filePath);
 }
 
-if (preg_match('/robot|spider|crawler|curl|^$/i', $_SERVER['HTTP_USER_AGENT']) && !isset($_SESSION['views'])) {
+if (!preg_match('/robot|spider|crawler|curl|^$/i', $_SERVER['HTTP_USER_AGENT']) && !isset($_SESSION['views'])) {
     $_SESSION['views'] = 1;
     fwrite(fopen($filePath, 'w'), $counter + 1);
     fwrite(fopen($hitLogPath, 'a'), $_SERVER['HTTP_USER_AGENT'] . '\n');

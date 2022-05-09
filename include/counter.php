f4a96d4a9c116ae3e9f630b5221a8091f80c7116
Cedric Seiz <cedricseiz@gmail.com>
2022-05-09 14:11:09 +0200

    Now hopefully ignoring crawler and bots in hitcount

--- include/counter.php
+++ include/counter.php
@@ -1,6 +1,7 @@
 <?php
 $counter = 1;
 $filePath = $_SERVER['DOCUMENT_ROOT'] . '/counter.txt';
+$hitLogPath = $_SERVER['DOCUMENT_ROOT'] . '/hitlog.txt';
 
 session_start();
 
@@ -10,5 +11,6 @@ if(file_exists($filePath)) {
 
 if (preg_match('/robot|spider|crawler|curl|^$/i', $_SERVER['HTTP_USER_AGENT']) && !isset($_SESSION['views'])) {
     $_SESSION['views'] = 1;
-    fwrite(fopen($filePath, 'w+'), $counter + 1);
+    fwrite(fopen($filePath, 'w'), $counter + 1);
+    fwrite(fopen($hitLogPath, 'a'), $_SERVER['HTTP_USER_AGENT'] . '\n');
 }

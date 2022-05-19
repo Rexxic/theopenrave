0d3450015eabff792954fe7e0c1e7a61b10339e9
Cedric Seiz <cedricseiz@gmail.com>
2022-05-19 18:16:00 +0200

    Extended hit log

--- analysis/stats.php
+++ analysis/stats.php
@@ -3,19 +3,21 @@ $servername = "localhost";
 $username = "mysql";
 $password = "Rex3141592653";
 $dbname = "hitlog";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+header("Content-Type: text/plain");
 
-$sql = "INSERT INTO hit values('" . $_SERVER['REMOTE_ADDR'] . "','" . $_SERVER['HTTP_USER_AGENT'] . "', default)";
+$query = 'SELECT distinct ipAddress,description,count(ipAddress) FROM hit';
 
-if (!$conn->query($sql) === TRUE) {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+$stmt = $conn->prepare($query);
+$stmt->execute();
+// Prozedural
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
 }
-
-$conn->close();
-?>

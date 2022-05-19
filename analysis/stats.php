23f1ad4fc3a43cee0c5b1390af65f86a39274b9c
Cedric Seiz <cedricseiz@gmail.com>
2022-05-19 19:52:00 +0200

    Expanded hitlog

--- analysis/stats.php
+++ analysis/stats.php
@@ -10,14 +10,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-header("Content-Type: text/plain");
 
-$query = 'SELECT distinct ipAddress,description, count(*) FROM hit group by ipAddress';
+$query = 'SELECT distinct ipAddress,description,count(*) FROM hit group by ipAddress';
 
 $stmt = $conn->prepare($query);
+if ($stmt === FALSE) {
+    die ("Error: " . $conn->error);
+}
 $stmt->execute();
 // Prozedural
 $result = $stmt->get_result();
+
+header("Content-Type: text/plain");
 while ($row = $result->fetch_assoc()) {
     echo str_replace("\n", "", print_r($row, true));
 }

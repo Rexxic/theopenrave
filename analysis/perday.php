a196afcc4864ba9293af2060f335f0e36ea94561
Cedric Seiz <cedricseiz@gmail.com>
2022-06-04 00:21:57 +0200

    Added perdaystat

--- analysis/perday.php
+++ analysis/perday.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$query = 'SELECT distinct date_format(timestamp,%Y-%m-%d),count(*) FROM hit group by date_format(timestamp,%Y-%m-%d) order by timestamp desc';
+$query = "SELECT distinct date_format(timestamp,'%Y-%m-%d'),count(*) FROM hit group by date_format(timestamp,'%Y-%m-%d') order by timestamp desc";
 
 $stmt = $conn->prepare($query);
 if ($stmt === FALSE) {

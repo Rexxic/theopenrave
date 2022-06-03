9f5c0a25a2e7819c3515130486ffcb6e95c2c44e
Cedric Seiz <cedricseiz@gmail.com>
2022-06-04 01:21:08 +0200

    Added perdaystat

--- include/loadflyer.php
+++ include/loadflyer.php
@@ -2,7 +2,7 @@
 if (!empty($dir)) {
     $scanned_dir = array_diff(scandir($_SERVER['DOCUMENT_ROOT'] . $dir), array('..', '.'));
     foreach ($scanned_dir as $file) {
-        if (strpos($file, ".png") || strpos($file, ".jpg") !== false) {
+        if (strpos($file, ".png") || strpos($file, ".jpg") || strpos($file, ".jpeg") !== false) {
             echo '<img class="flyer" src="' . $dir . '\\' . $file . '" alt="' . $file . '">';
         } elseif (strpos($file, ".php") !== false) {
             include($_SERVER['DOCUMENT_ROOT'] . $dir . '/' . $file);

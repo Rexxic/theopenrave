aa21ca1694c9ba88873e25b6f487783978b9def9
Cedric Seiz <cedricseiz@gmail.com>
2022-06-04 00:18:23 +0200

    Added perdaystat

---
+++ analysis/perday.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "mysql";
+$password = "Rex3141592653";
+$dbname = "hitlog";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$query = 'SELECT distinct date_format(timestamp,%Y-%m-%d),count(*) FROM hit group by date_format(timestamp,%Y-%m-%d) order by timestamp desc';
+
+$stmt = $conn->prepare($query);
+if ($stmt === FALSE) {
+    die ("Error: " . $conn->error);
+}
+$stmt->execute();
+// Prozedural
+$result = $stmt->get_result();
+
+header("Content-Type: text/plain");
+while ($row = $result->fetch_row()) {
+    echo $row[0] . ' - ' . $row[1] . "\n";
+}

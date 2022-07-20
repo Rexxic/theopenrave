ee159872fcf3fa94c1ac67633150ec4185a0e2c6
Cedric Seiz <cedricseiz@gmail.com>
2022-07-20 22:06:49 +0200

    Added language Metatag for Bing SEO

--- include/head.php
+++ include/head.php
@@ -1,11 +1,12 @@
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="content-language" content="de-de">
     <meta property="og:image" content="/media/TorLogo.png">
-    <meta property="og:description" content="Raves Heilbronn & Umgebung - Techno, HardTech, DnB">
-    <meta property="og:title" content="The Open Rave">
+    <meta property="og:description" content="Raves in Heilbronn & Umgebung - Techno, HardTech, DnB">
+    <meta property="og:title" content="The Open Rave - Heilbronn">
     <meta name="robots" content="max-image-preview:large">
     <meta name="description"
-          content="Raves Heilbronn & Umgebung - Techno, HardTech, DnB - Instagram: The Open ℝave (@theopenrave)">
+          content="Raves in Heilbronn & Umgebung - Techno, HardTech, DnB - Instagram: The Open ℝave (@theopenrave)">
     <title>The Open Rave</title>
     <link rel="stylesheet" href="/fonts/fonts.php">
     <link rel="stylesheet" href="/styles.css">

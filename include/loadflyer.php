12d749b26796ce1d8aa3ad4d934fa2c3ca88f660
Cedric Seiz <cedricseiz@gmail.com>
2022-10-12 11:13:30 +0200

    Added advanced sorting to loadflyer.php to correctly sort files in descending order but keep order for files of the same event

--- include/loadflyer.php
+++ include/loadflyer.php
@@ -1,6 +1,15 @@
 <?php
+function cmp($a, $b): int
+{
+    $a = substr($a, 0, strpos($a, '-'));
+    $b = substr($b, 0, strpos($b, '-'));
+    if ($a == $b) return 0;
+    return ($a > $b) ? -1 : 1;
+}
+
 if (!empty($dir)) {
     $scanned_dir = array_diff(scandir($_SERVER['DOCUMENT_ROOT'] . $dir), array('..', '.'));
+    usort($scanned_dir, 'cmp');
     foreach ($scanned_dir as $file) {
         if (strpos($file, ".png") || strpos($file, ".jpg") || strpos($file, ".jpeg") !== false) {
             echo '<img class="flyer" src="' . $dir . '\\' . $file . '" alt="' . $file . '">';

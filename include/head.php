ed51939c8e68bb27532e9a574b3d56d4f248ca23
Cedric Seiz <cedricseiz@gmail.com>
2023-09-26 09:41:45 +0200

    Some improvements

--- include/head.php
+++ include/head.php
@@ -1,9 +1,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="content-language" content="de-de">
-    <meta property="og:image" content="/media/TorLogo.png">
+    <meta property="og:image" content="https://theopenrave.de/media/TorLogo.png">
+    <meta property="og:url" content="https://theopenrave.de"
     <meta property="og:description" content="Raves in Heilbronn & Umgebung - Techno, HardTech, DnB">
     <meta property="og:title" content="The Open Rave - Heilbronn">
+    <meta property="og:locale" content="de_DE" />
     <meta name="robots" content="max-image-preview:large">
     <meta name="description"
           content="Raves in Heilbronn & Umgebung - Techno, HardTech, DnB - Instagram: The Open ℝave (@theopenrave)">
